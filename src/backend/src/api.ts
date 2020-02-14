c74938428c13e760e9d8fd7946a2764dd51ff20e
Nick Bova <npb7642@rit.edu>
2020-02-13 23:58:16 -0500

    post request doing something

--- src/backend/src/api.ts
+++ src/backend/src/api.ts
@@ -1,22 +1,37 @@
 import express from "express";
+import fs from "fs";
 const app = express();
 const port = 8000;
 
 const insults = require('../../resources/insults.json');
-const leaderboard = require('../../resources/leaderboard.json')
+let leaderboard = require('../../resources/leaderboard.json');
 
 app.get('/insults', (req, res) => {
-  // const data = req;
   return res.send(insults);
 });
 
 app.get('/leaderboard', (req, res) => {
-  // const data = req;
   return res.send(leaderboard);
 });
 
-app.put('/player', (req, res) => {
-  return res.send(leaderboard);
+app.post('/player', (req, res) => {
+  try{
+    fs.writeFileSync('../resources/leaderboard.json', JSON.stringify({...leaderboard, Player: [...leaderboard.Player, req.body]}));
+
+    leaderboard = {...leaderboard, Player: [...leaderboard.Player, req.body]};
+
+    return res.status(200).json({
+      status: 200,
+      message: 'Successfully updated file',
+      data: req.body
+    });
+  }
+  catch(e){
+    res.status(400).json({
+      status: 400,
+      error: e.message
+    });
+  }
 });
 
 // tslint:disable-next-line:no-console
