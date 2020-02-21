6bfb7fc18737b190564dcc99ff80c479f36aa56b
Guilherme Dias <gldias97@gmail.com>
2020-02-20 19:56:37 -0500

    Get 5 random insults from backend

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,28 @@ import {
 interface MyProps {};
 interface MyState {
   testState: string,
+  insults: string[],
 };
 
 // TODO: this is just here until the routing and everything is set up, so it should be removed eventually
-let insults = ["You are dumb", "Your father was a hamster and your mother smelt of elderberries", "I dislike you"];
+// let insults = ["You are dumb", "Your father was a hamster and your mother smelt of elderberries", "I dislike you"];
 
 class App extends React.Component<MyProps, MyState> {
   constructor(props: any) {
     super(props);
     this.state = {
       testState: "save",
+      insults: [],
     }
+  }
+
+  componentDidMount() {
+    fetch('/insults')
+      .then(res => res.json())
+      .then(insults => {
+          const shuffled = insults.Insults.sort(() => 0.5 - Math.random());
+          this.setState({insults: shuffled.slice(0, 5)});
+      });
   }
 
   render(): JSX.Element {
@@ -37,7 +48,7 @@ class App extends React.Component<MyProps, MyState> {
               <LandingPage />
             </Route>
             <Route exact path={'/fight'}>
-              <TypingPage insults={insults} />
+              <TypingPage insults={this.state.insults} />
             </Route>
             <Route exact path={'/leaderboard'}>
               <LeaderboardPage />
