1c88e9ceccde216977e31cba351061ca69080b86
Ryan Connors <connorsryanj@gmail.com>
2020-02-24 10:31:09 -0500

    Added backend integration to leaderboard

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -2,7 +2,49 @@ import React from 'react';
 import './Leaderboard.css';
 import Table from 'react-bootstrap/Table'
 
-export class Leaderboard extends React.Component {
+export interface LeaderboardProps {}
+
+export interface LeaderboardState {
+    records: Array<Record>;
+}
+
+export interface Record {
+    name: string;
+    time: number;
+}
+
+export class Leaderboard extends React.Component<LeaderboardProps, LeaderboardState> {
+    constructor(props: LeaderboardProps) {
+        super(props);
+
+        this.state = {
+            records: []
+        };
+
+        this.generateLeaderboard = this.generateLeaderboard.bind(this);
+    }
+
+    componentDidMount() {
+        fetch('/leaderboard')
+            .then(res => res.json())
+            .then(ranks => {
+                this.setState({records: ranks.Player});
+            });
+    }
+
+    generateLeaderboard() {
+        console.log(this.state.records);
+        return this.state.records.map((record, index) => { 
+            return (
+            <tr>
+                <td>{ index }</td>
+                <td>{ record.name }</td>
+                <td>{ record.time }</td>
+            </tr>) 
+         });
+    }
+
+
     render() {
       return (
           <div className="leaderboard-container">
@@ -18,56 +60,7 @@ export class Leaderboard extends React.Component {
                     </thead>
                     {/*Hard Coded Currently until design is finalized*/}
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>RJC</td>
-                            <td>1:24:45</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>RJC</td>
-                            <td>1:30:15</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>RJC</td>
-                            <td>1:30:16</td>
-                        </tr>
-                        <tr>
-                            <td>4</td>
-                            <td>RJC</td>
-                            <td>1:34:45</td>
-                        </tr>
-                        <tr>
-                            <td>5</td>
-                            <td>RJC</td>
-                            <td>1:36:15</td>
-                        </tr>
-                        <tr>
-                            <td>6</td>
-                            <td>RJC</td>
-                            <td>1:38:16</td>
-                        </tr>
-                        <tr>
-                            <td>7</td>
-                            <td>RJC</td>
-                            <td>1:40:45</td>
-                        </tr>
-                        <tr>
-                            <td>8</td>
-                            <td>RJC</td>
-                            <td>1:42:15</td>
-                        </tr>
-                        <tr>
-                            <td>9</td>
-                            <td>RJC</td>
-                            <td>1:43:16</td>
-                        </tr>
-                        <tr>
-                            <td>10</td>
-                            <td>RJC</td>
-                            <td>1:50:16</td>
-                        </tr>
+                        {this.generateLeaderboard()}
                     </tbody>
                 </Table>
               </div>
