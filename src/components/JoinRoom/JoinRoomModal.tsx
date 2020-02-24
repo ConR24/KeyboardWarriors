b48447a4f00d59d52d0ae0143decf10912f2798c
Nick Bova <npb7642@rit.edu>
2020-02-24 13:37:55 -0500

    files for each modal being added

---
+++ src/components/JoinRoom/JoinRoomModal.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import "./FinishModal.css";
+import Form from "react-bootstrap/Form";
+import Button from 'react-bootstrap/Button';
+import {Link} from 'react-router-dom';
