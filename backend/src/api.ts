97eae59ecef43a790ef650749387e868e889cd91
Alan19 <Alan19@users.noreply.github.com>
2020-02-28 09:53:54 -0500

    Comments in api+socket.ts and disconnect handler

--- backend/src/api.ts
+++ backend/src/api.ts
@@ -15,7 +15,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const insults = require('../../src/resources/insults.json');
 let leaderboard = require('../../src/resources/leaderboard.json');
 
+//Define Socket.IO functions
 sio.on('connection', (client: SocketIO.Socket) => {
+  /**
+   * Join a room with a room code. Can only join room with less than 2 people.
+   */
   client.on('joinRoom', (roomCode: string) => {
     const room = sio.nsps['/'].adapter.rooms[roomCode];
     if (!room) {
@@ -30,37 +34,68 @@ sio.on('connection', (client: SocketIO.Socket) => {
     }
   });
 
+  /**
+   * Send an insult to everyone else in  the room
+   */
   client.on('sendInsult', (roomCode: string, insult: string) => {
     client.broadcast.to(roomCode).emit('incomingInsult', insult);
   });
 
+  /**
+   * Send a message to everyone else in the room to leave and have the client leave the room
+   */
   client.on('leaveRoom', (roomCode: string) => {
     client.broadcast.to(roomCode).emit('playerLeftRoom');
     client.leave(roomCode);
   });
+
+  /**
+   * Make player broadcast they are leaving from every room they are in when disconnecting
+   */
+  client.on('disconnect', (reason: string) => {
+    Object.keys(client.rooms).forEach(roomCode => {
+      client.broadcast.to(roomCode).emit('playerLeftRoom');
+      client.leave(roomCode);
+    });
+  })
 });
 
+/**
+ * Sort the leaderboard in ascending order
+ */
 function sortLeaderboard(){
   leaderboard.Player.sort(function(a: any, b: any){
     return a.time - b.time;
 });
 }
 
+/**
+ * Restrict the size of the leaderboard to top 10 players
+ */
 function resizeLeaderboard(){
   if(leaderboard.Player.length > 10){  // keep a max number of 10 scores
     leaderboard.Player.splice(10, leaderboard.Player.length - 1);
   }
 }
 
+/**
+ * Send 5 random insults to player
+ */
 app.get('/insults', (req: Request, res: Response) => {
   const shuffled = insults.Insults.sort(() => 0.5 - Math.random());
   return res.send(shuffled.slice(0, 5));
 });
 
+/**
+ * Send leaderboard to front end
+ */
 app.get('/leaderboard', (req: Request, res: Response) => {
   return res.send(leaderboard);
 });
 
+/**
+ * Write player 'score' to leaderboard and resize.
+ */
 app.post('/player', (req: Request, res: Response) => {
   try{
     leaderboard = {...leaderboard, Player: [...leaderboard.Player, req.body]};  // add the new score
