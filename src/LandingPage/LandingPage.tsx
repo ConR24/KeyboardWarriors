038b5205700194a8ce7aa81dec1a7733571743c9
Guilherme Dias <gldias97@gmail.com>
2020-02-28 10:17:18 -0500

    Remove old code

--- src/LandingPage/LandingPage.tsx
+++ src/LandingPage/LandingPage.tsx
@@ -5,14 +5,12 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import {Link} from 'react-router-dom';
-import { subscribeToTimer } from '../scripts/socket';
 import "./index.css";
 
 export class LandingPage extends React.Component {
 
   componentDidMount() {
     document.title = "Keyboard Warriors";
-    subscribeToTimer((err: any, timestamp: any) => console.log(timestamp));
   }
 
   render() {
