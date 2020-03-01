73c45bb968072c03a42d09bbbc13bb1557be72a6
Nick Bova <npb7642@rit.edu>
2020-03-01 12:59:18 -0500

    removed bad modals

--- src/components/MakeRoom/MakeRoomModal.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Modal from 'react-bootstrap/Modal'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import "./FinishModal.css";
-import Form from "react-bootstrap/Form";
-import Button from 'react-bootstrap/Button';
-import {Link} from 'react-router-dom';
