947c61110398502a52359b4df5a0b7c44fc10fdb
gldias <gldias97@gmail.com>
2020-03-03 02:01:13 +0000

    Deploying to gh-pages from  - d18d22e6f8cf6442eccbebf40e8f86adf59a6d97 🚀

---
+++ precache-manifest.ef74572ffb9d9ea4d41ecc436ff5b29b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99e8ffc4b14088dd43b28ab05189157",
+    "url": "/KeyboardWarriors/index.html"
+  },
+  {
+    "revision": "33f75113f5c00dd272df",
+    "url": "/KeyboardWarriors/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "649648f0b8f5d278afb0",
+    "url": "/KeyboardWarriors/static/css/main.627326d6.chunk.css"
+  },
+  {
+    "revision": "33f75113f5c00dd272df",
+    "url": "/KeyboardWarriors/static/js/2.de555d48.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/KeyboardWarriors/static/js/2.de555d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649648f0b8f5d278afb0",
+    "url": "/KeyboardWarriors/static/js/main.d6eef381.chunk.js"
+  },
+  {
+    "revision": "8ed51875b999bd8c3147",
+    "url": "/KeyboardWarriors/static/js/runtime-main.dbc262a9.js"
+  },
+  {
+    "revision": "81f452a923d9560d5c025acecea5a90f",
+    "url": "/KeyboardWarriors/static/media/badJob.81f452a9.png"
+  },
+  {
+    "revision": "d6d9d913b065948249bfbfd698885ae4",
+    "url": "/KeyboardWarriors/static/media/keyboardWarriorWhite.d6d9d913.png"
+  },
+  {
+    "revision": "f043fd9d9933c531b799a2dafa795a95",
+    "url": "/KeyboardWarriors/static/media/keyboardWarriors.f043fd9d.png"
+  }
+]);
