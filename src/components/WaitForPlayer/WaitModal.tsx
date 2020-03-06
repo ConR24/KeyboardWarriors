f51c3ed806fec8ae5d9375c3d985f55baa7b7362
Lauren <ljcrafty@comcast.net>
2020-03-06 09:43:09 -0500

    Initial WaitModal

--- src/components/WaitForPlayer/WaitModal.tsx
+++ src/components/WaitForPlayer/WaitModal.tsx
@@ -6,3 +6,18 @@ import "./FinishModal.css";
 import Form from "react-bootstrap/Form";
 import Button from 'react-bootstrap/Button';
 import {Link} from 'react-router-dom';
+
+export interface WaitProps {
+    show: boolean;
+}
+
+export class WaitModal extends React.Component<WaitProps> {
+    render() {
+        return (
+            <Modal show={this.props.show}>
+                <h1>Waiting</h1>
+                <p>Waiting for opponent to join</p>
+            </Modal>
+        );
+    }
+}
